381088ca14580d59c73df28c7827742fda36def0
Norgius <dmitry.schadrin21@yandex.ru>
2022-10-13 20:11:20 +0300

    Clear code

--- datacenter/active_passcards_view.py
+++ datacenter/active_passcards_view.py
@@ -3,8 +3,7 @@ from django.shortcuts import render
 
 
 def active_passcards_view(request):
-    all_passcards = Passcard.objects
-    active_passcards = all_passcards.filter(is_active=True)
+    active_passcards = Passcard.objects.filter(is_active=True)
     context = {
         'active_passcards': active_passcards,
     }
