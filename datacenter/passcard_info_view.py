72b0da0a630fa188bd0735fdd00a6309c8cfbdeb
Norgius <dmitry.schadrin21@yandex.ru>
2022-10-13 23:04:19 +0300

    Merged imports

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,5 +1,4 @@
-from datacenter.models import Passcard, Visit
-from datacenter.models import format_duration, is_visit_long
+from datacenter.models import Passcard, Visit, format_duration, is_visit_long
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 
